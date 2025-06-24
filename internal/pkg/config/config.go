8dde7cae9867e79a5a288a8fba5c47150605bcac
yzqzss <yzqzss@yandex.com>
2025-06-24 17:58:57 +0800

    feat: headless!

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -79,6 +79,21 @@ type Config struct {
 	UseHQ                  bool     // Special field to check if HQ is enabled depending on the command called
 	HQRateLimitingSendBack bool     `mapstructure:"hq-rate-limiting-send-back"`
 
+	// Headless
+	Headless            bool   `mapstructure:"headless"`
+	HeadlessTrace       bool   `mapstructure:"headless-trace"`
+	ChroumiumBin        string `mapstructure:"chromium-bin"`
+	Headfull            bool   `mapstructure:"headfull"`
+	DevTools            bool   `mapstructure:"dev-tools"`
+	StealthMode         bool   `mapstructure:"stealth-mode"`
+	HeadlessUserDataDir string `mapstructure:"headless-user-data-dir"`
+
+	PageLoadTimeout time.Duration `mapstructure:"page-load-timeout"`
+	PostLoadDelay   time.Duration `mapstructure:"post-load-delay"`
+
+	Behaviors       string        `mapstructure:"behaviors"`
+	BehaviorTimeout time.Duration `mapstructure:"behavior-timeout"`
+
 	// Network
 	Proxy         string `mapstructure:"proxy"`
 	RandomLocalIP bool   `mapstructure:"random-local-ip"`
